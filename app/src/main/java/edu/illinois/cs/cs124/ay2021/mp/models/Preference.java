8b9980577c1a64f60935d5143fbab5bfeb454b3d
rudhibashambu <rudhib2@illinois.edu>
2021-12-02 20:40:34 -0600

    MP2

--- app/src/main/java/edu/illinois/cs/cs124/ay2021/mp/models/Preference.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2021/mp/models/Preference.java
@@ -2,8 +2,7 @@ package edu.illinois.cs.cs124.ay2021.mp.models;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Preference extends AppCompatActivity {
-  public Preference() {}
+public class Preference {
 
   private String id;
 

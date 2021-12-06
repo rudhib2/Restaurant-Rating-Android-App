cc54260ddcd300d33f59777fcbc80135afc9db5f
rudhibashambu <rudhib2@illinois.edu>
2021-12-05 18:41:06 -0600

    MP3- PT1

--- app/src/main/java/edu/illinois/cs/cs124/ay2021/mp/models/RelatedRestaurants.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2021/mp/models/RelatedRestaurants.java
@@ -3,8 +3,11 @@ package edu.illinois.cs.cs124.ay2021.mp.models;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 // import java.util.prefs.Preferences;
 
@@ -12,11 +15,13 @@ public class RelatedRestaurants {
   private Map<String, Map<String, Integer>> restaurantRelationships = new HashMap<>();
 
   private List<String> validIDs = new ArrayList<>();
+  private List<Restaurant> restaurantList = new ArrayList<>();
 
   public RelatedRestaurants(
       final List<Restaurant> restaurants, final List<Preference> preferences) {
     for (Restaurant res : restaurants) {
       validIDs.add(res.getId());
+      restaurantList.add(res);
     }
     for (Restaurant restaurant : restaurants) {
       Map<String, Integer> restaurantPreferenceMap = new HashMap<>();
@@ -48,5 +53,38 @@ public class RelatedRestaurants {
       return mapReturn;
     }
     return new HashMap<>();
+  }
+
+  public List<Restaurant> getRelatedInOrder(final String restaurantId) {
+    if (restaurantId == null || !(validIDs.contains(restaurantId))) {
+      throw new IllegalArgumentException();
+    }
+    Map<String, Integer> mapRelated = getRelated(restaurantId);
+    Set keySet = mapRelated.keySet().stream().collect(Collectors.toSet());
+    List<String> listRelated = new ArrayList<>();
+    for (Object key : keySet) {
+      String key2 = (String) key;
+      listRelated.add(key2);
+    }
+    List<Restaurant> listNowRelated = new ArrayList<>();
+    for (int i = 0; i < restaurantList.size(); i++) {
+      if (listRelated.contains(restaurantList.get(i).getId())) {
+        listNowRelated.add(restaurantList.get(i));
+      }
+    }
+    Collections.sort(
+        listNowRelated,
+        (r1, r2) -> {
+          return r1.getName().compareTo(r2.getName());
+        });
+    Collections.sort(
+        listNowRelated,
+        (r1, r2) -> {
+          return mapRelated.get(r2.getId()) - mapRelated.get(r1.getId());
+        });
+    return listNowRelated;
+  }
+  public Set<Restaurant> getConnectedTo(final String restaurantID) {
+    return new HashSet<>();
   }
 }
